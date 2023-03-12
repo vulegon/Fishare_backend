c3c6bc2cfa79b6351b73c49f3e1158f670e7ffab
ikebata_yuki <walkurepqrt@gmail.com>
2023-03-12 10:06:44 +0900

    一部テストの修正

--- spec/models/spot_spec.rb
+++ spec/models/spot_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Spot,type: :model do
+RSpec.describe Spot, type: :model do
   describe '#valid' do
     context '緯度が入力されていないとき' do
       let!(:spot) { FactoryBot.build(:spot, longitude: 123 ) }
