a9bb02c63b8c1af1e98bb7f62fa9c09e81ed626f
ikebata_yuki <walkurepqrt@gmail.com>
2023-07-04 07:49:44 +0900

    不要な部分の削除

--- app/controllers/api/v1/users/registrations_controller.rb
+++ app/controllers/api/v1/users/registrations_controller.rb
@@ -3,20 +3,15 @@ module Api
     module Users
       class RegistrationsController < ApplicationController
         # ユーザーを作成します
-        # @param name[<String>] 入力した名前 
-        # @param email[<String>] 入力したメールアドレス
-        # @param password[<String>] 入力したパスワード
-        # @return message[<String>] 実行結果のメッセージ
+        # POST api/v1/users
         def create
           create_params = ::Users::Registrations::CreateParameter.new(params)
 
           if create_params.invalid?
             render_parameter_error(create_params) and return
           end
-
-          ::Users::RegistrationService.create!(create_params)
       
-          json = { message: 'ユーザー登録が完了しました' }
+          json = { message: 'ユーザー登録のメールを送信しました。' }
           render status: :ok, json: json
         end
       end
--- app/parameters/users/registrations/create_parameter.rb
+++ app/parameters/users/registrations/create_parameter.rb
@@ -4,16 +4,11 @@ module Users
       include ActiveModel::Model
       include ActiveModel::Attributes
 
-      attribute :name, :string
       attribute :email, :string
-      attribute :password, :string
 
-      validates :name, presence: true, length: { maximum: 20 }
       validates :email, presence: true
-      validates :password, presence: true, length: { minimum: 8, maximum: 128 }
       validate :email_is_not_registered
       validate :email_must_be_valid_format, if: -> { email.present? }
-      validate :password_must_be_valid_format, if: -> { password.present? }
 
       def initialize(params)
         super(params.permit(self.class.attribute_names))
@@ -34,12 +29,6 @@ module Users
         return if email =~ ::User::VALID_EMAIL_REGEX
 
         errors.add(:email, 'は正しいメールアドレスの形式ではありません')
-      end
-
-      def password_must_be_valid_format
-        return if password =~ ::User::VALID_PASSWORD_REGEX
-
-        errors.add(:password, 'は英小文字と数字の両方を含める必要があります')
       end
     end
   end
