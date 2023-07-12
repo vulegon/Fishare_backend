81be30295689eabdac193c524a1d471ebf8b598a
ikebata_yuki <walkurepqrt@gmail.com>
2023-07-13 08:38:44 +0900

    serviceクラスの作成

---
+++ app/services/users/registration_service.rb
@@ -0,0 +1,17 @@
+module Users
+  class RegistrationService
+    class << self
+      # メール認証済みでないユーザーを作成し、指定したメールアドレスにメールを送信します。
+      # @param params[<::Users::Registrations::CreateParameter>] ユーザー作成に必要なパラメータ
+      # @return void
+      def create_user!(params)
+        ActiveRecord::Base.transaction do
+          confirmation_token = ::User.set_email_confirmation
+          user = User.new(email: params.email, confirmation_token: confirmation_token)
+          user.save!(validate: false)
+          ::UserMailer.send_email_confirmation(user).deliver
+        end
+      end
+    end
+  end
+end
---
+++ spec/services/users/registration_service_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe Users::RegistrationService, type: :service do
+  describe '.create_user!' do
+    subject { described_class.create_user!(create_params) }
+    let(:create_params) { ::Users::Registrations::CreateParameter.new(params) }
+    let(:params) { ActionController::Parameters.new(email: email) }
+    let(:email) { 'example@example.com' }
+  
+    before(:each) do
+      allow(UserMailer).to receive(:send_email_confirmation).and_return(double("mail", deliver: true))
+    end
+
+    context 'メール認証していないユーザーが作成されていること。メールが送られるメソッドが実行されていること' do
+      it {
+        expect(UserMailer).to receive(:send_email_confirmation)
+        expect{ subject }.to change{ User.exists?(email: email) }.from(false).to(true)
+       }
+    end
+  end
+end
