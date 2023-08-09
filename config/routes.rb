b0dc9e735e91cb6f28809d217f4bfd2444b408fd
ikebata_yuki <walkurepqrt@gmail.com>
2023-08-10 07:21:45 +0900

    NOT　NULL追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,6 @@ Rails.application.routes.draw do
   namespace :api, constraints:{ format: 'json' } do
     namespace :v1 do
       resources :spots, only: [:index, :create]
-
-      devise_for :users, controllers: {
-        registrations: 'api/v1/users/registrations',
-        sessions: 'api/v1/users/sessions'
-      }
     end
   end
 end
