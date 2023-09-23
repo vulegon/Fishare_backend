facfba2717118915e4fa558e78f65b298131509b
ikebata_yuki <walkurepqrt@gmail.com>
2023-09-23 14:31:58 +0900

    細かい部分の修正

--- app/controllers/api/v1/auth/sessions_controller.rb
+++ app/controllers/api/v1/auth/sessions_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     module Auth
-      class SessionsController < DeviseTokenAuth::SessionsController
+      class SessionsController < ApplicationController
         def index
           if current_api_v1_user
               render json: {is_login: true, data: current_api_v1_user }
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api, constraints:{ format: 'json' } do
     namespace :v1 do
       resources :spots, only: [:index, :create, :show]
-      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+      mount_devise_token_auth_for 'User', at: 'auth',skip: [:omniauth_callbacks], controllers: {
         registrations: 'api/v1/auth/registrations',
         sessions: 'api/v1/auth/sessions',
         passwords: 'api/v1/auth/passwords',
