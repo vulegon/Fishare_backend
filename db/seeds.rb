76ad336c16141324bc6173008de2a9de0029713b
ikebata_yuki <walkurepqrt@gmail.com>
2023-10-04 06:58:48 +0900

    bulk_insertのgemを導入

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,11 @@
-positions = [
-  { latitude: 36.20127926543816, longitude: 136.12792391726268 },
-  { latitude: 35.62157403594883, longitude: 140.05411136023233 },
-  { latitude: 36.04049680286872, longitude: 140.37267630908482 },
-  { latitude: 35.72085365236546, longitude: 139.92741562402446 },
-]
-
-Spot.create!(positions)
-
-(1..10).each_with_index do |user, i|
-  name = "tester#{i}"
-  password = "Password#{1}"
-  email = "example#{i}@example.com"
-  User.create!(name: name, password: password, email: email)
+ActiveRecord::Base.transaction do
+  (1..10).each_with_index do |user, i|
+    name = "tester#{i}"
+    password = "Password#{1}"
+    email = "example#{i}@example.com"
+    User.create!(name: name, password: password, email: email)
+  end
 end
-
 fish_master = [
   { id: 1, name: "カサゴ", type: "海" },
   { id: 2, name: "ニジマス", type: "川" },
@@ -95,6 +87,7 @@ fish_master = [
   { id: 77, name: "ヨシノボリ", type: "海" },
   { id: 78, name: "ワカシ", type: "海" },
 ]
-fish_names = fish_master.map { |fish| fish[:name] }
-ActiveRecord::Base.transaction do
-end
+fish = fish_master.map { |fish| Fish.new(name: fish[:name]) }
+
+  Fish.import fish
+
