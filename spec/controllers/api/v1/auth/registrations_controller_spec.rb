9882bc807384db77c7d308a32b3c307ef4ad002c
ikebata_yuki <walkurepqrt@gmail.com>
2023-10-21 19:57:06 +0900

    一旦コミット

---
+++ spec/controllers/api/v1/auth/registrations_controller_spec.rb
@@ -0,0 +1,54 @@
+require "rails_helper"
+
+RSpec.describe Api::V1::Auth::RegistrationsController, type: :request do
+  describe "POST #create" do
+    subject {
+      post api_v1_user_registration_path, params: params
+      response
+    }
+    let(:user) { FactoryBot.create(:user) }
+    let(:auth_headers) { user.create_new_auth_token }
+    let(:params) { { name: name, email: email, password: password, password_confirmation: password_confirmation, confirm_success_url: confirm_success_url } }
+    let(:name) { 'test1' }
+    let(:email) { 'tester1-1@fishare.com' }
+    let(:password) { 'Password1' }
+    let(:password_confirmation) { password }
+    let(:confirm_success_url) { 'http://localhost:3000' }
+
+    context "パラメーターが正しいとき" do
+      it {
+        should have_http_status(:ok)
+        expect(response["access-token"].present?).to eq(true)
+        expect(response["client"].present?).to eq(true)
+        expect(response["uid"]).to eq(email)
+      }
+    end
+
+    context "パラメーターが間違っているとき" do
+      context "名前が誤りのとき" do
+        let(:name) { '' }
+        it { should have_http_status(:unprocessable_entity) }
+      end
+
+      context "メールアドレスが誤りのとき" do
+        let(:email) { '' }
+        it { should have_http_status(:unprocessable_entity) }
+      end
+
+      context "パスワードが誤りのとき" do
+        let(:password) { '' }
+        it { should have_http_status(:unprocessable_entity) }
+      end
+
+      context "パスワード確認が誤りのとき" do
+        let(:password_confirmation) { '' }
+        it { should have_http_status(:unprocessable_entity) }
+      end
+
+      context "リダイレクトURLが誤りのとき" do
+        let(:confirm_success_url) { nil }
+        it { should have_http_status(:unprocessable_entity) }
+      end
+    end
+  end
+end
