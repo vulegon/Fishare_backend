d727d3aeb339b358831212bc90a336473dbba209
ikebata_yuki <walkurepqrt@gmail.com>
2023-11-11 05:48:15 +0900

    locationの設定

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,5 +1,19 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Location, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#valid?" do
+    subject { FactoryBot.build(:location, name: name) }
+
+    context "名前が有効な値のとき" do
+      let(:name) { "#{Location::NAMES.first}" }
+      it { should be_valid }
+    end
+
+    context "名前が無効な値のとき" do
+      context "名前がNAMESにはない値のとき" do
+        let(:name) { "invalid_name" }
+        it { should be_invalid }
+      end
+    end
+  end
 end
